16384c805f18c74a59598c17ea11a17a7e861f86
Liis U <liis.usin@gmail.com>
2022-10-24 14:26:33 +0200

    Check if the Sentence Is Pangram

---
+++ src/com/liu0001/Check_if_the_Sentence_Is_Pangram_1832/Main.java
@@ -0,0 +1,30 @@
+package com.liu0001.Check_if_the_Sentence_Is_Pangram_1832;
+
+/*
+1832. Check if the Sentence Is Pangram.
+
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only
+lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        boolean ans = solution.checkIfPangram(sentence);
+        System.out.println(ans);
+
+    }
+}
+
+
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+
+        return (sentence.chars().distinct().count() >= 26);
+    }
+}
